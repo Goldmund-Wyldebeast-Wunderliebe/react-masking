8e9bddfbcafdec3dee31f0f9faea790d1ff26af1
Kenneth Veldman <veldman@gw20e.com>
2017-01-30 11:02:13 +0100

    Updated build component

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,69 @@
 "use strict";
 var webpack = require('webpack');
 var path = require('path');
-var loaders = require('./webpack.loaders');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const HOST = process.env.HOST || "127.0.0.1";
 const PORT = process.env.PORT || "8888";
 
 module.exports = {
-    entry: [
-        `webpack-dev-server/client?http://${HOST}:${PORT}`,
-        `webpack/hot/only-dev-server`,
-        `./examples/index.jsx` // Your appʼs entry point
+  entry: [
+    `webpack-dev-server/client?http://${HOST}:${PORT}`,
+    `webpack/hot/only-dev-server`,
+    `./examples/index.jsx` // Your appʼs entry point
+  ],
+  devtool: process.env.WEBPACK_DEVTOOL || 'cheap-module-source-map',
+  output: {
+    path: path.join(__dirname, 'docs'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    modulesDirectories: ['node_modules', 'bower_components'],
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components|public)/,
+        loaders: ['react-hot-loader/webpack']
+      },
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.json$/,
+        loader: 'json'
+      }
     ],
-    devtool: process.env.WEBPACK_DEVTOOL || 'cheap-module-source-map',
-    output: {
-        path: path.join(__dirname, 'docs'),
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extensions: ['', '.js', '.jsx']
-    },
-    module: {
-        loaders
-    },
-    devServer: {
-        contentBase: "./docs",
-        // do not print bundle build stats
-        noInfo: true,
-        // enable HMR
-        hot: true,
-        // embed the webpack-dev-server runtime into the bundle
-        inline: true,
-        // serve index.html in place of 404 responses to allow HTML5 history
-        historyApiFallback: true,
-        port: PORT,
-        host: HOST
-    },
-    plugins: [
-        new webpack.NoErrorsPlugin(),
-        new webpack.HotModuleReplacementPlugin(),
-        new HtmlWebpackPlugin({
-            template: './examples/template.html'
-        })
+    preLoaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components|public)/,
+        loader: 'eslint-loader'
+      }
     ]
+  },
+  devServer: {
+    contentBase: "./docs",
+    // do not print bundle build stats
+    noInfo: true,
+    // enable HMR
+    hot: true,
+    // embed the webpack-dev-server runtime into the bundle
+    inline: true,
+    // serve index.html in place of 404 responses to allow HTML5 history
+    historyApiFallback: true,
+    port: PORT,
+    host: HOST
+  },
+  plugins: [
+    new webpack.NoErrorsPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: './examples/template.html'
+    })
+  ]
 };
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,38 +1,61 @@
 'use strict';
 const path = require('path');
 const webpack = require('webpack');
-var loaders = require('./webpack.loaders');
 
 
 module.exports = {
   entry: './src/react-masking.jsx',
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'react-masking.js'
+    filename: 'react-masking.js',
+    libraryTarget: 'umd'
   },
   resolve: {
+    modulesDirectories: ['node_modules', 'bower_components'],
     extensions: ['', '.js', '.jsx']
   },
   module: {
-    loaders
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.json$/,
+        loader: 'json'
+      }
+    ],
+    preLoaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components|public)/,
+        loader: 'eslint-loader'
+      }
+    ]
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env':{
+      'process.env': {
         'NODE_ENV': JSON.stringify('production')
       }
     }),
     new webpack.NoErrorsPlugin(),
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin({
-      compress:{
+      compress: {
         warnings: true
       }
     }),
     new webpack.optimize.AggressiveMergingPlugin(),
   ],
   externals: {
-    'react': 'react',
-    'react-dom' : 'reactDOM'
+    react: {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react'
+    },
+    'react-dom': 'reactDOM'
   }
 };
